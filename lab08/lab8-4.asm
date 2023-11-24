ddfe5a1d8efccb9222e6eaa25097d463e6b44ee6
geloginov1 <gorgloginovgit@gmail.com>
2023-11-25 02:57:58 +0300

    Add files for lab08

---
+++ lab08/lab8-4.asm
@@ -0,0 +1,36 @@
+%include 'in_out.asm'
+
+SECTION .data
+f_x db "функция: f(x)=3(10 + x)",0h
+msg db 10,13,'результат: ',0h
+
+SECTION .text
+global _start
+
+_start:
+pop ecx
+pop edx
+sub ecx, 1
+mov esi, 0
+
+next:
+cmp ecx,0h
+jz _end
+pop eax
+call atoi
+add eax, 10
+mov ebx, 3
+mul ebx
+add esi, eax
+
+loop next
+
+_end:
+mov eax, f_x
+call sprint
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+
+call quit
